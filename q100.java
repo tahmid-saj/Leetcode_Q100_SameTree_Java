b06985f73f7bc9971045ff1fb1854b39eddd34ae
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2023-05-20 17:18:12 -0400

    Create q100.java

---
+++ q100.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (helper(p, q) == false) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean helper(TreeNode p, TreeNode q) {
+        if (p != null && q != null) {
+            if (helper(p.left, q.left) == false) {
+                return false;
+            }
+
+            if (p.val != q.val) {
+                return false;
+            }
+
+            if (helper(p.right, q.right) == false) {
+                return false;
+            }
+        }
+
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+
+        return true;
+    }
+}
